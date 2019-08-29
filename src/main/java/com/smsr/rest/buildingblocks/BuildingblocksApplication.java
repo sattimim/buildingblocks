0838633f904a5560114aed9565fa6f3133fe55f0
Madhu Satti <sattim.im@gmail.com>
2019-08-29 13:35:47 -0700

    First Commit

---
+++ src/main/java/com/smsr/rest/buildingblocks/BuildingblocksApplication.java
@@ -0,0 +1,13 @@
+package com.smsr.rest.buildingblocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingblocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingblocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smsr/rest/buildingblocks/BuildingblocksApplicationTests.java
@@ -0,0 +1,16 @@
+package com.smsr.rest.buildingblocks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BuildingblocksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
