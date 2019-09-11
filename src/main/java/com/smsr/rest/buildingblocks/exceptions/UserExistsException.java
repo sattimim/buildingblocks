c665b222f528c68945c29935850362a49507b64a
Madhu Satti <sattim.im@gmail.com>
2019-09-11 13:51:59 -0700

    04-ExceptionHandling-ResponseCodes development completed

---
+++ src/main/java/com/smsr/rest/buildingblocks/exceptions/UserExistsException.java
@@ -0,0 +1,14 @@
+package com.smsr.rest.buildingblocks.exceptions;
+
+public class UserExistsException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+
+	public UserExistsException(String message) {
+		super(message);
+		
+	}
+	
+	
+	
+}
--- src/main/java/com/smsr/rest/buildingblocks/services/UserService.java
+++ src/main/java/com/smsr/rest/buildingblocks/services/UserService.java
@@ -4,9 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.smsr.rest.buildingblocks.entities.User;
+import com.smsr.rest.buildingblocks.exceptions.UserExistsException;
+import com.smsr.rest.buildingblocks.exceptions.UserNotFoundException;
 import com.smsr.rest.buildingblocks.repositories.UserRepository;
 
 @Service
@@ -19,24 +23,48 @@ public class UserService {
 		return userRepository.findAll();
 	}
 	
-	public User createUser(User user) {
-		return userRepository.save(user);
+	public User createUser(User user) throws UserExistsException{
+		User existingUser = userRepository.findByUsername(user.getUsername());
+		
+		if(existingUser != null ) {
+			throw new UserExistsException("User already exists in the repository");
+		} else {
+			return userRepository.save(user);
+		}
 	}
 	
-	public Optional<User> getUserById(Long id) {
+	public Optional<User> getUserById(Long id) throws UserNotFoundException {
 		
 		Optional<User> user = userRepository.findById(id);
+		
+		if(!user.isPresent()) {
+			throw new UserNotFoundException("User Not Found");
+		}
 		return user;
 	}
 	
-	public User updateUserById(Long id, User user) {
-		user.setId(id);
-		return userRepository.save(user);
+	public User updateUserById(Long id, User user) throws UserNotFoundException {
+		
+		Optional<User> findUser = userRepository.findById(id);
+		
+		if (!findUser.isPresent()) {
+			throw new UserNotFoundException("User Not Found");
+		} else {
+		
+			user.setId(id);
+			return userRepository.save(user);
+		}
 		
 	}
 	
 	public void deleteUserById(Long id) {
-		if(userRepository.findById(id).isPresent()) {
+		
+		Optional<User> user = userRepository.findById(id);
+		
+		
+		if(!user.isPresent()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User not found");
+		} else  {
 			userRepository.deleteById(id);
 		}
 	}
